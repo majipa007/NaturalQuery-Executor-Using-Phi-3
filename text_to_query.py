af45c1e0f6aac32e7b0aa5e8a58e61592f660ced
sulav kumar shrestha <sulavstha007@gmail.com>
2024-07-20 22:58:44 +0530

    text to query is converted from procedural to object oriented

---
+++ text_to_query.py
@@ -0,0 +1,36 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, BitsAndBytesConfig
+
+
+class text_to_query:
+    def __init__(self):
+        quantization_config = BitsAndBytesConfig(load_in_4bit=True)
+
+        self.model = AutoModelForCausalLM.from_pretrained("Majipa/text-to-SQL",
+                                                          device_map="cuda",
+                                                          torch_dtype="auto",
+                                                          quantization_config=quantization_config)
+
+        self.tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-4k-instruct")
+        self.pipe = pipeline(
+            "text-generation",
+            model=self.model,
+            tokenizer=self.tokenizer,
+        )
+        self.generation_args = {
+            "max_new_tokens": 500,
+            "temperature": 0.7,
+            "return_full_text": False,
+        }
+
+    def query(self, inp):
+        messages = [
+            {"role": "system", "content": "You are a helpful text-to-SQL assistant."},
+            {"role": "user",
+             "content": f"question:{inp} context:CREATE TABLE contacts (contact_id INTEGER PRIMARY KEY,first_name TEXT "
+                        f"NOT NULL,last_name TEXT NOT NULL,email TEXT NOT NULL UNIQUE,phone TEXT NOT NULL UNIQUE)"},
+        ]
+        output = self.pipe(messages, **self.generation_args)
+        print(output[0]['generated_text'])
+
+x = text_to_query()
+x.query("show me the whole data base")
